bb2a681f95d4d159a73af7d16a5762eb9d9e47e1
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2021-03-25 18:40:10 +0530

    Updated resources and added filter on gallery page

--- script/gallery.js
+++ script/gallery.js
@@ -1,52 +1,13 @@
 noOfImages = 12
-
+image_category = ['ai_lang_pro', 'workshop', 'meteor', 'ai_catechism', 'innopreneurs', 'prize_distribution']
 const renderImages = ()=>{
 
-    let introTag = document.getElementById('images-row-intro')
-    let workshopTag = document.getElementById('images-row-workshop')
-    let meteorTag = document.getElementById('images-row-meteor')
-    let aicatTag = document.getElementById('images-row-aicat')
-    let innoTag = document.getElementById('images-row-inno')
-    let prizeTag = document.getElementById('images-row-prize')
+    let imageRowTag = document.getElementById('images-row')
 
-    let introTemplate = ``
-    let workshopTemplate = ``
-    let meteorTemplate = ``
-    let aicatTemplate = ``
-    let innoTemplate = ``
-    let prizeTemplate = ``
+    let imageTemplate = ``
     for (let i = 1; i <= noOfImages; i++) {
-        introTemplate += `<div class="col-lg-4 col-md-4 col-xs-6 thumb">
-            <a href="images/gallery/${i}.jpg" class="fancybox" rel="ligthbox">
-                <img  src="images/gallery/${i}.jpg" class="zoom img-fluid "  alt="">
-            
-            </a>
-        </div>`
-        workshopTemplate += `<div class="col-lg-4 col-md-4 col-xs-6 thumb">
-            <a href="images/gallery/${i}.jpg" class="fancybox" rel="ligthbox">
-                <img  src="images/gallery/${i}.jpg" class="zoom img-fluid "  alt="">
-            
-            </a>
-        </div>`
-        meteorTemplate += `<div class="col-lg-4 col-md-4 col-xs-6 thumb">
-            <a href="images/gallery/${i}.jpg" class="fancybox" rel="ligthbox">
-                <img  src="images/gallery/${i}.jpg" class="zoom img-fluid "  alt="">
-            
-            </a>
-        </div>`
-        aicatTemplate += `<div class="col-lg-4 col-md-4 col-xs-6 thumb">
-            <a href="images/gallery/${i}.jpg" class="fancybox" rel="ligthbox">
-                <img  src="images/gallery/${i}.jpg" class="zoom img-fluid "  alt="">
-            
-            </a>
-        </div>`
-        innoTemplate += `<div class="col-lg-4 col-md-4 col-xs-6 thumb">
-            <a href="images/gallery/${i}.jpg" class="fancybox" rel="ligthbox">
-                <img  src="images/gallery/${i}.jpg" class="zoom img-fluid "  alt="">
-            
-            </a>
-        </div>`
-        prizeTemplate += `<div class="col-lg-4 col-md-4 col-xs-6 thumb">
+        let category = image_category[(i-1)%image_category.length]
+        imageTemplate += `<div class="col-lg-4 col-md-4 col-xs-6 thumb filter ${category}">
             <a href="images/gallery/${i}.jpg" class="fancybox" rel="ligthbox">
                 <img  src="images/gallery/${i}.jpg" class="zoom img-fluid "  alt="">
             
@@ -55,12 +16,7 @@ const renderImages = ()=>{
 
     }
 
-    introTag.innerHTML += introTemplate
-    workshopTag.innerHTML += workshopTemplate
-    meteorTag.innerHTML += meteorTemplate
-    aicatTag.innerHTML += aicatTemplate
-    innoTag.innerHTML += innoTemplate
-    prizeTag.innerHTML += prizeTemplate
+    imageRowTag.innerHTML += imageTemplate
 
 
 }
